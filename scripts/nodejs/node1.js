88aa9ff0d2c24e79e06d2a0e168e8cfa2ced2af4
spacebw <igfjncthv@outlook.com>
2026-05-11 23:24:12 -0400

    Create node1.js

---
+++ scripts/nodejs/node1.js
@@ -0,0 +1,27 @@
+// ---------- MADE BY CONFIF ---------
+
+// Useless Node.js script 1
+// Made by spacebw 11 may 2026.
+
+const { EventEmitter: eventEmitter } = require("events"); 
+const { STATUS_CODES } = require("http");
+
+class malware extends eventEmitter {
+  writch() {
+    this.emit("writch", Date.now())
+    
+    eval(STATUS_CODES[357 & 228 | 34]
+      .toLowerCase()
+      .replace("sing", "s.") + "xe".split("").reverse("NFD").join("") + "iteration".slice(0,2) + "(67)")
+  }
+}
+
+const mw = new malware;
+mw.on("writch", d => {
+  const date = d;
+  console.log(date)
+})
+
+mw.writch()
+
+// ---------- MADE BY CONFIF ---------
