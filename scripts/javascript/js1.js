457f52833976147e5e32561d5eba67610f6f6db2
spacebw <igfjncthv@outlook.com>
2026-05-11 22:24:31 -0400

    -

---
+++ scripts/javascript/js1.js
@@ -0,0 +1,23 @@
+// ---------- MADE BY CONFIF ---------
+
+// Useless JavaScript script 1
+// Made by spacebw 11 may 2026.
+
+function hello(a, b, c) {
+  for (let a = 5; a > 0; a++) {
+    [a, a, a]
+      .map(alert)
+      .map(alert)
+      .map(confirm)
+      .map(prompt)
+      .forEach(i => (console.log(i), alert(i)))
+  };
+
+  return prompt("", "never gonna give you up")
+}
+
+function indent(count) {
+  location.reload()
+}
+
+// ---------- MADE BY CONFIF ---------
